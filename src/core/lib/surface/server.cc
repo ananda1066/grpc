34fde6dac5931de9cf6c9f7361246a15b10429af
ananda1066 <ananda1066@users.noreply.github.com>
2023-09-19 21:49:19 +0000

    Automated change: Fix sanity tests

--- src/core/lib/surface/server.cc
+++ src/core/lib/surface/server.cc
@@ -52,7 +52,6 @@
 #include "src/core/lib/channel/channelz.h"
 #include "src/core/lib/config/core_configuration.h"
 #include "src/core/lib/experiments/experiments.h"
-#include "src/core/lib/gpr/useful.h"
 #include "src/core/lib/gprpp/crash.h"
 #include "src/core/lib/gprpp/debug_location.h"
 #include "src/core/lib/gprpp/match.h"
